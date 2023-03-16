e3d712d7ccc32ec1826e1199fce3adc4cc079588
EDmitryBJ <edv.busines@gmail.com>
2023-03-16 18:25:14 +0500

    Init

---
+++ src/main/java/com/vstavaystrana/vstavaystrana_site/VstavaystranaSiteApplication.java
@@ -0,0 +1,13 @@
+package com.vstavaystrana.vstavaystrana_site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VstavaystranaSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VstavaystranaSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vstavaystrana/vstavaystrana_site/VstavaystranaSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vstavaystrana.vstavaystrana_site;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VstavaystranaSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
