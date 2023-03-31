5fb40b0ed2378e004a51de415ef2387195dc6984
stlad <82213506+stlad@users.noreply.github.com>
2023-03-31 20:08:05 +0500

    Более гибкое создание пользователя с формы

--- src/main/java/com/vstavaystrana/vstavaystrana_site/controllers/RegistrationController.java
+++ src/main/java/com/vstavaystrana/vstavaystrana_site/controllers/RegistrationController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -21,12 +22,13 @@ public class RegistrationController {
 
 
     @GetMapping("/registration")
-    public String registration()
+    public String registration(Model model)
     {
+        model.addAttribute("user", new User());
         return "registration";
     }
     @PostMapping("/registration")
-    public String adduser(User user, Model model)
+    public String adduser(@ModelAttribute("user") User user, Model model)
     {
         try
         {
