6addb5d66a20e239df587b6130549de6ceb25c02
stlad <82213506+stlad@users.noreply.github.com>
2023-04-13 17:37:18 +0500

    связи между таблицами

--- src/main/java/com/vstavaystrana/vstavaystrana_site/models/Buisnessman.java
+++ src/main/java/com/vstavaystrana/vstavaystrana_site/models/Buisnessman.java
@@ -1,11 +1,20 @@
 package com.vstavaystrana.vstavaystrana_site.models;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 
 @Entity
 public class Buisnessman extends AbstractEntity
 {
-    protected Long userId;
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+    //protected Long userId;
+
+
+
     protected Byte[] passportScan;
     protected Float readiness;
     protected Boolean haveCompletedProject;
--- src/main/java/com/vstavaystrana/vstavaystrana_site/models/News.java
+++ src/main/java/com/vstavaystrana/vstavaystrana_site/models/News.java
@@ -1,11 +1,17 @@
 package com.vstavaystrana.vstavaystrana_site.models;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 
 @Entity
 public class News extends AbstractEntity
 {
-    protected Long projectId;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "project_id", referencedColumnName = "id")
+    protected Project project;
     protected String author;
     protected String content;
     protected String photoUrl;
