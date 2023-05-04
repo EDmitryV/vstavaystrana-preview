7699b0d9a793318b01f3058da33ebcf4653f900b
semyon-v-dot <semyon-v-dot@yandex.com>
2023-05-04 15:13:57 +0500

    Init bot

---
+++ src/main/java/com/vstavaystrana/vstavaystrana_site/bot/TelegramBot.java
@@ -0,0 +1,36 @@
+package com.vstavaystrana.vstavaystrana_site.bot;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+public class TelegramBot extends TelegramLongPollingBot {
+    private final String botUsername;
+
+    public TelegramBot() {
+        super(Dotenv.load().get("BOT_TOKEN"));
+        botUsername = Dotenv.load().get("BOT_NAME");
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            SendMessage message = new SendMessage();
+            message.setChatId(update.getMessage().getChatId().toString());
+            message.setText(update.getMessage().getText());
+
+            try {
+                execute(message);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public String getBotUsername() { return botUsername; }
+}
