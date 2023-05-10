0529936800378c8e8217488256cf1a1dec077a49
semyon-v-dot <semyon-v-dot@yandex.com>
2023-05-10 21:00:34 +0500

    Add findByProject method for NewsRepository

--- src/main/java/com/vstavaystrana/vstavaystrana_site/repositories/NewsRepository.java
+++ src/main/java/com/vstavaystrana/vstavaystrana_site/repositories/NewsRepository.java
@@ -1,8 +1,11 @@
 package com.vstavaystrana.vstavaystrana_site.repositories;
 
 import com.vstavaystrana.vstavaystrana_site.models.News;
+import com.vstavaystrana.vstavaystrana_site.models.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 public interface NewsRepository extends JpaRepository<News, Long> {
+    List<News> findByProject(Project proj);
 }
